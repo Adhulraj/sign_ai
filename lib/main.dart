d35b62f918c077e9dbab8ec3de2e10f533899dee
Adhulraj K R <adhulrajkr@gmail.com>
2024-02-15 21:09:17 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,177 @@
+// ignore_for_file: file_names
+
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:sign_ai/websocket.dart';
+// import 'package:sign_ai/styles.dart';
+
+void main() => runApp(const MaterialApp(
+  home: VideoStream(),
+));
+
+class VideoStream extends StatefulWidget {
+  const VideoStream({Key? key}) : super(key: key);
+
+  @override
+  State<VideoStream> createState() => _VideoStreamState();
+}
+
+class _VideoStreamState extends State<VideoStream> {
+  final WebSocket _socket = WebSocket("ws://localhost:5000");
+  bool _isConnected = false;
+  void connect(BuildContext context) async {
+    _socket.connect();
+    setState(() {
+      _isConnected = true;
+    });
+  }
+
+  void disconnect() {
+    _socket.disconnect();
+    setState(() {
+      _isConnected = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Live Video"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () => connect(context),
+                    // style: buttonStyle,
+                    child: const Text("Connect"),
+                  ),
+                   ElevatedButton(
+                    onPressed: disconnect,
+                    // style: buttonStyle,
+                    child: const Text("Disconnect"),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 50.0,
+              ),
+              _isConnected
+                  ? StreamBuilder(
+                      stream: _socket.stream,
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return const CircularProgressIndicator();
+                        }
+
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          return const Center(
+                            child: Text("Connection Closed !"),
+                          );
+                        }
+                        //? Working for single frames
+                        return Image.memory(
+                          Uint8List.fromList(
+                            base64Decode(
+                              (snapshot.data.toString()),
+                            ),
+                          ),
+                          gaplessPlayback: true,
+                          excludeFromSemantics: true,
+                        );
+                      },
+                    )
+                  : const Text("Initiate Connection")
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'SignBridge',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: MyHomePage(title: 'SignBridge Home Page'),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key? key, this.title}) : super(key: key);
+
+//   final String? title;
+
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title!),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Expanded(
+//               child: Container(
+//                 color: Colors.grey,
+//                 child: const Center(child: Text('Video')),
+//               ),
+//             ),
+//             Expanded(
+//               child: Container(
+//                 color: Colors.purple,
+//                 child: const Center(child: Text('Text')),
+//               ),
+//             ),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: <Widget>[
+//                 ElevatedButton(
+//                   onPressed: () {},
+//                   style: ElevatedButton.styleFrom(
+//                     backgroundColor: Colors.green,
+//                   ),
+//                   child: const Text('Start'),
+//                 ),
+//                 ElevatedButton(
+//                   onPressed: () {},
+//                   style: ElevatedButton.styleFrom(
+//                     backgroundColor: Colors.red,
+//                   ),
+//                   child: const Text('Stop'),
+//                 ),
+//               ],
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
